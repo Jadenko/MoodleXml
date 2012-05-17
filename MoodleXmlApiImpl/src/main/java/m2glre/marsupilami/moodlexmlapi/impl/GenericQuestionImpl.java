dbb594a7e24295e8f3036cf054cdbc07c610b9f7
Mohamed ETTAH <mohamed.ettah@gmail.com>
2012-05-18 01:55:24 +0200

    med modif

---
+++ MoodleXmlApiImpl/src/main/java/m2glre/marsupilami/moodlexmlapi/impl/GenericQuestionImpl.java
@@ -0,0 +1,17 @@
+package m2glre.marsupilami.moodlexmlapi.impl;
+
+import m2glre.marsupilami.moodlexmlapi.core.data.GenericQuestion;
+
+public class GenericQuestionImpl extends GenericQuestion {
+	
+	
+	public GenericQuestionImpl() {
+	}
+
+	public Boolean isHidden() {
+		return getIsHidden();
+	}
+
+	
+
+}

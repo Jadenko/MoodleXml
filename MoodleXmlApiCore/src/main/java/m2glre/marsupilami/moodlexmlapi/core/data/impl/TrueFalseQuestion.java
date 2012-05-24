92b761451e45bb6001b04aab650bd408c6ad3cff
Mohamed ETTAH <mohamed.ettah@gmail.com>
2012-05-24 09:30:47 +0200

    Remove jad modif on the api core

--- MoodleXmlApiCore/src/main/java/m2glre/marsupilami/moodlexmlapi/core/data/impl/TrueFalseQuestion.java
+++
@@ -1,7 +0,0 @@
-package m2glre.marsupilami.moodlexmlapi.core.data.impl;
-
-import m2glre.marsupilami.moodlexmlapi.core.data.GenericQuestion;
-
-public class TrueFalseQuestion extends GenericQuestion {
-
-}
--- MoodleXmlApiImpl/src/main/java/m2glre/marsupilami/moodlexmlapi/presenter/Dispatcher.java
+++
@@ -1,8 +0,0 @@
-package m2glre.marsupilami.moodlexmlapi.presenter;
-
-import m2glre.marsupilami.moodlexmlapi.core.QuizImportExportService;
-
-public class Dispatcher {
-	QuizImportExportService quizImportExportService ;
-	
-}

2653ef6ae3df14e9d8525a2fb781351b3096ab29
Jad Bouchouka <jadairline@hotmail.com>
2012-05-28 13:54:51 +0200

    faire un fetch pour ramener les modifs de momo

--- MoodleXmlApiImpl/src/main/java/m2glre/marsupilami/moodlexmlapi/presenter/QuizImportExportServiceImpl.java
+++ MoodleXmlApiImpl/src/main/java/m2glre/marsupilami/moodlexmlapi/presenter/QuizImportExportServiceImpl.java
@@ -2,14 +2,12 @@ package m2glre.marsupilami.moodlexmlapi.presenter;
 
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.InputStream;
 import java.io.OutputStream;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Marshaller;
-import javax.xml.bind.Unmarshaller;
 
 import m2glre.marsupilami.moodlexmlapi.core.InvalidQuizFormatException;
 import m2glre.marsupilami.moodlexmlapi.core.InvalidStreamSizeException;
@@ -28,26 +26,6 @@ public class QuizImportExportServiceImpl implements QuizImportExportService {
 			throws InvalidQuizFormatException, InvalidStreamSizeException {
 		// TODO Auto-generated method stub
 		IImportedQuiz importedQuiz = null;
-		
-		JAXBContext context;
-		try {
-			context = JAXBContext.newInstance(Quiz.class);
-		
-		Unmarshaller um = context.createUnmarshaller();
-		Quiz quizImported = (Quiz) um.unmarshal(new FileReader("./quiz-jaxb.xml"));
-
-			for (int i = 0; i < quizImported.getQuestionList().toArray().length; i++) {
-				System.out.println("Question " + (i + 1) + ": "
-						+ quizImported.getQuestionList().get(i).getName() + " from "
-						+ quizImported.getQuestionList().get(i).getImageUrl());
-			}
-		} catch (JAXBException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (FileNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
 		
 		return importedQuiz;
 	}
